8e2309b7434f61e50b09e1a3348fd659c4a93188
vjain25 <90483086+vjain25@users.noreply.github.com>
2022-04-01 21:20:17 -0400

    Update Banker.java
    Fixed minor Customer method calls and java issues.

--- Banker.java
+++ Banker.java
@@ -1,5 +1,7 @@
+import java.util.*;
+
 public class Banker{
-  
+
     private String firstName; // first name
     private String lastName; // last name
     private String username;
@@ -8,72 +10,73 @@ public class Banker{
 
     public Banker(String f, String l, String u, String p){ // constructor for the Banker
 
-      firstName = f;
-      lastName = l;
-      username = u;
-      password = p;
-      
-    }
-  
-    public void createEntry(Customer C, String method){ // adds an entry to the record array list: method is the name of the method
-     
-      HashMap<String, String> rec = new HashMap<>();
-      rec.put(C.getUserName(), method);
-      record.add(rec);
-      
-    }
-  
-    public void createCard(Customer C){ 
-   
-      if(C.hasCreditCard()) // If they already have a credit card
-        throw new IllegalArgumentException("Already has a credit card");
-      if(C.getAge() < 18) // Minors can't have cards
-        throw new IllegalArgumentException("Underage");
-      C.setCreditCard(true);
-      
-      createEntry(C, "createCard");
-    
-    }
-  
-    public void deleteCard(Customer C){
-     
-      if(!C.hasCreditCard()) // They don't have one anyway
-        throw new IllegalArgumentException("No Card Found");
-      if(C.getCreditCardDue()!=0) // They need to settle all their bills before closing it
-        throw new IllegalArgumentException("Bills not paid");
-      C.setCreditCard(false);
-      
-      createEntry(C, "deleteCard");
+        firstName = f;
+        lastName = l;
+        username = u;
+        password = p;
 
     }
-  
-    public void freeze(Customer C){
-     
-      if(C.getIsFrozen()) // The accoutn is already frozen
-        throw new IllegalArgumentException("Account already frozen");
-      C.setIsFrozen(true);
-      createEntry(C, "freeze");
-      
+
+    public void createEntry(Customer C, String method){ // adds an entry to the record array list: method is the name of the method
+
+        HashMap<String, String> rec = new HashMap<>();
+        rec.put(C.getUsername(), method);
+        record.add(rec);
+
     }
-  
+
+    public void createCard(Customer C){
+
+        if(C.hasCreditCard()) // If they already have a credit card
+            throw new IllegalArgumentException("Already has a credit card");
+        if(C.getAge() < 18) // Minors can't have cards
+            throw new IllegalArgumentException("Underage");
+        C.setCreditCard(true);
+
+        createEntry(C, "createCard");
+
+    }
+
+    public void deleteCard(Customer C){
+
+        if(!C.hasCreditCard()) // They don't have one anyway
+            throw new IllegalArgumentException("No Card Found");
+        if(C.getCreditCardDue()!=0) // They need to settle all their bills before closing it
+            throw new IllegalArgumentException("Bills not paid");
+        C.setCreditCard(false);
+
+        createEntry(C, "deleteCard");
+
+    }
+
+    public void freeze(Customer C){
+
+        if(C.isFrozen()) // The accoutn is already frozen
+            throw new IllegalArgumentException("Account already frozen");
+        C.setFrozen(true);
+        createEntry(C, "freeze");
+
+    }
+
     public void defreeze(Customer C){
 
-        if(!C.getIsFrozen()) // The account was already defrozen
-          throw new IllegalArgumentException("Account already defrozen");
-        C.setIsFrozen(false);
+        if(!C.isFrozen()) // The account was already defrozen
+            throw new IllegalArgumentException("Account already defrozen");
+        C.setFrozen(false);
         createEntry(C, "defreeze");
-      
-     }
 
+    }
 
     public void transfer(Customer C1, Customer C2, double money){ // transfer money between accounts
-     
-      if(C1.getBalance() < money) // If the balance isn't enough to transfer
-        throw new IllegalArgumentException("Not enough balance");
-      C2.newTransaction("Account transfer from" + C1.getUsername(), money); // Put money into C2
-      C1.newTransaction("Account transfer to" + C1.getUsername(), money); // Take money out of C1
-      createEntry(C, "transfer");
-      
+
+        if(C1.getBalance() < money) // If the balance isn't enough to transfer
+            throw new IllegalArgumentException("Not enough balance");
+        C2.newTransaction("Account transfer from" + C1.getUsername(), money); // Put money into C2
+        C1.newTransaction("Account transfer to" + C1.getUsername(), money); // Take money out of C1
+        createEntry(C1, "transferout");
+        createEntry(C2, "transferin");
+
+
     }
-    
+
 }
