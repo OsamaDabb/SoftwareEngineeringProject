9f87990950bd7f7fc7f5089108fca5acbf5e582a
OsamaDabb <76997940+OsamaDabb@users.noreply.github.com>
2022-04-01 23:03:14 -0400

    Modified loadData() to expect user data on separate lines

--- main.java
+++ main.java
@@ -1,5 +1,8 @@
+import com.opencsv.*;
+
 import java.util.*;
 import java.io.*;
+
 
 
 public class main{
@@ -10,8 +13,8 @@ public class main{
         }
         ArrayList<Banker> Bankers = Data[0];
         ArrayList<Customer> Customers = Data[1];
-        System.out.println(Data[0]);
-        System.out.println(Data[1]);
+        System.out.println(Bankers);
+        System.out.println(Customers);
     }
 
     //method to open catalogue csv file, stores all customer and banker data into ArrayLists on memory
@@ -28,42 +31,44 @@ public class main{
         ArrayList<Banker> Bankers = new ArrayList<Banker>();
 
         //seperate csv file by commas
-        scan.useDelimiter(",");
+        scan.useDelimiter("\n");
         while(scan.hasNext()){
-            int itemID = scan.nextInt();
+            String[] item = scan.nextLine().split(",");
+
+            int itemID = Integer.parseInt(item[0]);
             //control branch for banker data
+            //bankers expect 4 input variables
             if(itemID == 0){
                 //simple string/int/bool scans from csv file for basic banker data
-                String u = scan.next();
-                String p = scan.next();
-                String f = scan.next();
-                String l = scan.next();
+                String u = item[1];
+                String p = item[2];
+                String f = item[3];
+                String l = item[4];
                 //call constructor on new banker then add banker to array of Bankers
                 Banker newBanker = new Banker(u, p, f, l);
                 Bankers.add(newBanker);
             }
             //control branch for customers
+            //customers expect 11 inputs of data
             else if(itemID == 1){
                 //simple string/int/bool scans from csv file for basic user data
-                String u = scan.next();
-                String p = scan.next();
-                String f = scan.next();
-                String l = scan.next();
-                int a = scan.nextInt();
-                boolean froz = scan.nextBoolean();
-                double b = scan.nextDouble();
-                boolean credit = scan.nextBoolean();
-                double creditDue = scan.nextDouble();
+                String u = item[1];
+                String p = item[2];
+                String f = item[3];
+                String l = item[4];
+                int a = Integer.parseInt(item[5]);
+                boolean froz = Boolean.parseBoolean(item[6]);
+                double b = Double.parseDouble(item[7]);
+                boolean credit = Boolean.parseBoolean(item[8]);
+                double creditDue = Double.parseDouble(item[9]);
 
                 //seperating csv for transactions (stored as: transID;transVal;transID;transVal pairs)
-                String[] transactionList = scan.next().split(";");
+                String[] transactionList = item[10].split(";");
                 ArrayList<HashMap<String,Double>> transHash = new ArrayList<>();
                 //array that pairs transaction IDs and transaction values in a ArrayList of HashMaps
                 for(int i = 0; i < transactionList.length;i += 2){
                     String transName = transactionList[i];
-                    System.out.println(transName);
                     double transValue = Double.parseDouble(transactionList[i+1]);
-                    System.out.println(transValue);
                     HashMap<String,Double> n = new HashMap<>();
                     n.put(transName, transValue);
                     transHash.add(n);
@@ -71,8 +76,16 @@ public class main{
 
                 //queries which are saved as query.query.query string on csv file, are seperated by
                 // .split(".") and saved as Array
-                String[] queryList = scan.next().split(".");
-                ArrayList<String> queries = new ArrayList<>(List.of(queryList));
+                String quer = item[11];
+                ArrayList<String> queries;
+                //queries are saved as "None" in saveData() if a user has no queries
+                if(quer == "None"){
+                    queries = new ArrayList<>();
+                }
+                else {
+                    String[] queryList = quer.split(".");
+                    queries = new ArrayList<>(List.of(queryList));
+                }
 
                 //adds new customer to ArrayList Customers
                 Customer newCustomer = new Customer(u,p,f,l,a,froz,b,credit,creditDue,transHash, queries);
@@ -84,8 +97,35 @@ public class main{
         return new ArrayList[] {Bankers, Customers};
     }
 
-    public void saveData(){
+    public void saveData( ArrayList<Banker> Bankers, ArrayList<Customer> Customers){
+        File file = new File("catalogue.csv");
+        try {
+            // create FileWriter object with file as parameter
+            FileWriter outputfile = new FileWriter(file);
 
+            // create CSVWriter object filewriter object as parameter
+            CSVWriter writer = new CSVWriter(outputfile);
+
+            // adding header to csv
+            String[] header = { "Name", "Class", "Marks" };
+            writer.writeNext(header);
+
+            // add data to csv
+            String[] data1 = { "Aman", "10", "620" };
+            writer.writeNext(data1);
+            String[] data2 = { "Suraj", "10", "630" };
+            writer.writeNext(data2);
+
+            // closing writer connection
+            writer.close();
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        for(int i = 0;i < Customers.size(); i++){
+
+        }
 
     }
 }
